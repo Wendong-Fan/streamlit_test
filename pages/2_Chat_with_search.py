4f72ae0a84ae81c108f68757bbd25db3f0734d2f
Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>
2023-08-04 00:17:32 +0800

    Update 2_Chat_with_search.py

--- pages/2_Chat_with_search.py
+++ pages/2_Chat_with_search.py
@@ -11,8 +11,8 @@ with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
  
 st.title("🧐 小佛陀")
-# image = Image.open('images.jpeg')
-# st.image(image)
+image0 = Image.open('images.jpeg')
+st.image(image0)
 
 
 if "messages" not in st.session_state:
