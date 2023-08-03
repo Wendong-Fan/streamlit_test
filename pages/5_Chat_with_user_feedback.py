897d41dfbc8b31e6f49dc189a42ff47d16f5f481
Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>
2023-08-03 23:09:48 +0800

    Update 5_Chat_with_user_feedback.py

--- pages/5_Chat_with_user_feedback.py
+++ pages/5_Chat_with_user_feedback.py
@@ -7,11 +7,11 @@ with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     
-st.title("🔎 Trubrics - Chat with user feedback")
+st.title("🔎 Trubrics - Chat with me)
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "assistant", "content": "Hi, how can I help you? Leave feedback to help my team understand my weaknesses!"}
+        {"role": "assistant", "content": "Hi, how can I help you? 你好，有什么我可以帮你的嘛？"}
     ]
 if "response" not in st.session_state:
     st.session_state["response"] = None
