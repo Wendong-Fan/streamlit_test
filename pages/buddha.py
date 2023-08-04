cf5cd1333428d80300d8b075a3ec5eb21f86bdd2
Wendong-Fan <133094783+Wendong-Fan@users.noreply.github.com>
2023-08-04 15:55:16 +0800

    Update buddha.py

--- pages/buddha.py
+++ pages/buddha.py
@@ -40,4 +40,18 @@ if prompt := st.chat_input(placeholder="宇宙的起源是什么"):
         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
         response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
         st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response)reopenai-mutiagent.openai.azure.com/",
+                        openai_api_version = "2023-03-15-preview",
+                        openai_api_key = key1,
+                        openai_api_type = "azure",
+                        deployment_name="gpt-35-turbo",
+                        model_name="gpt-35-turbo", 
+                        streaming=True)
+
+    search = DuckDuckGoSearchRun(name="Search")
+    search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+        st.session_state.messages.append({"role": "assistant", "content": response})
         st.write(response)
